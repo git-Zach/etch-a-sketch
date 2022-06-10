6181cda273df5468ce1897471c0db8e85d910ceb
Zach <szachary0808@outlook.com>
2022-06-09 15:07:53 -1000

    Created rainbow button functionality

--- logic.js
+++ logic.js
@@ -1,6 +1,3 @@
-//const height = container.clientHeigth;
-//const width = container.clientWidth;
-
 const rangeSlider = document.getElementById("rangeSlider");
 const output = document.getElementById("demo");
 output.innerHTML = rangeSlider.value;
@@ -11,13 +8,17 @@ const clearGrid = document.getElementById("clearGrid");
 let   rowAndCol = 16;
 let cellWidth = (width / rowAndCol) - 2;
 let cellHeight = (height / rowAndCol) - 2;
+const rainbowColor = document.getElementById("rainbowColor");
+
+
+/* CREATION */
 
 function makeRows(rows, cols, cellHeight, cellWidth) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    //cell.innerText = (i + 1);
+    
     cell.style.height = `${cellHeight}px`
     cell.style.width = `${cellWidth}px`
     
@@ -27,14 +28,31 @@ function makeRows(rows, cols, cellHeight, cellWidth) {
 };
 
 makeRows(rowAndCol, rowAndCol,cellHeight, cellWidth);
+let gridItem = document.querySelectorAll('#griditem')
+colorBlack();
+
+rainbowColor.addEventListener('click', () => {
+    if (rainbowColor.style.backgroundColor != '') {
+        rainbowColor.style.backgroundColor =  '';
+        colorBlack();
+    }
+    else {
+        makeRainbow();
+        rainbowColor.style.backgroundColor = '#47B5FF'
+    }
+});
+
+clearGrid.addEventListener('click', clearGrid1)
+/* DELETION */
+
 function deleteRows() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
 
-let gridItem = document.querySelectorAll('#griditem')
-color();
+
+
 rangeSlider.oninput = function() {
     output.innerHTML = this.value;
     rowAndCol = this.value;
@@ -45,18 +63,51 @@ rangeSlider.oninput = function() {
     deleteRows();
     makeRows(rowAndCol, rowAndCol, cellHeight, cellWidth)
     gridItem = document.querySelectorAll('#griditem')
-    color();
+    colorBlack();
     
 }
 
-    clearGrid.addEventListener('click', clearGrid1)
 
-function color() { 
+
+/* COLORS */
+function colorBlack() { 
     gridItem.forEach(gridItem =>
-    gridItem.addEventListener('mouseover', () => gridItem.classList.add('active')
-))};
+        gridItem.addEventListener('mouseover', () => 
+            gridItem.style.backgroundColor = '#000000'))
+};
+
+
+function getRandomColor() {
+    const availableCharacters = '0123456789ABCDEF';
+    const availableCharactersLength = availableCharacters.length;
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += availableCharacters[Math.floor(Math.random() * availableCharactersLength)];
+    }
+
+    return color;
+}
+
+
+
+function randomColor(getRandomColor) {
+    gridItem.forEach(gridItem => 
+        gridItem.addEventListener('mouseover', () =>
+            gridItem.style.backgroundColor = getRandomColor()));
+        
+};
+
+
+
+
+
+function makeRainbow() {
+    randomColor(getRandomColor);
+}
+
 
 function clearGrid1() {
-    gridItem.forEach(gridItem => gridItem.classList.remove('active'))
+    gridItem.forEach(gridItem => gridItem.style.backgroundColor = '#ddd')
 }
 
